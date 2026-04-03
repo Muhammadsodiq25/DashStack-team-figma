786f97b5893b5a1890c8337aa76e27e6cee6d1df
abdulbositKing <abdulbosit48798105@gmail.com>
2026-04-03 08:32:16 +0500

    my page

---
+++ src/data/Abdulbosit.js
@@ -0,0 +1,54 @@
+import CalendarImg from "../assets/Calendar-img.png";
+
+const Abdulbosit = [
+  {
+    page: "calendar",
+
+    title: "Calender",
+    buttonText: "+ Add New Event",
+    goingTo: "You are going to",
+
+    events: [
+      {
+        id: 1,
+        title: "Design Conference",
+        time: "Today 07:19 AM",
+        location: "56 Davion Mission Suite 157",
+        country: "Meaghenberg",
+        people: 15,
+        color: "#6C5CE7",
+      },
+      {
+        id: 2,
+        title: "Weekend Festival",
+        time: "16 October 2026 at 5:00 PM",
+        location: "853 Moore Flats Suite 158",
+        country: "Sweden",
+        people: 20,
+        color: "#E84393",
+      },
+      {
+        id: 3,
+        title: "Glastonbury Festival",
+        time: "20-22 October 2026 at 8:00 PM",
+        location: "646 Walter Road Apt. 571",
+        country: "Turks Caicos Islands",
+        people: 14,
+        color: "#F39C12",
+      },
+      {
+        id: 4,
+        title: "Ultra Europe 2026",
+        time: "25 October at 10:00 PM",
+        location: "506 Satterfield Tunnel Apt. 963",
+        country: "San Marino",
+        people: 42,
+        color: "#0984E3",
+      },
+    ],
+
+    calendarImage: CalendarImg,
+  },
+];
+
+export default Abdulbosit;
--- src/pages/calendar/Calendar.jsx
+++ src/pages/calendar/Calendar.jsx
@@ -1,10 +1,65 @@
+import "./Calendar.css";
+import Abdulbosit from "../../data/Abdulbosit";
 
 const Calendar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const calendarData = Abdulbosit.find((item) => item.page === "calendar");
 
-export default Calendar
+  return (
+    <div className="calender">
+      <div className="calender__top">
+        <h2>{calendarData.title}</h2>
+      </div>
+
+      <div className="calender__bottom">
+        <div className="calender__bottom--left">
+          <div className="bottom__left--top">
+            <button>{calendarData.buttonText}</button>
+            <h3>{calendarData.goingTo}</h3>
+          </div>
+
+          <div className="bottom__left--cards">
+            {calendarData.events.map((item) => (
+              <div className="event__card" key={item.id}>
+                <div className="dot" style={{ background: item.color }}></div>
+
+                <div className="event__info">
+                  <h4>{item.title}</h4>
+                  <p>{item.time}</p>
+                  <p>{item.location}</p>
+                  <span>{item.country}</span>
+                </div>
+
+                <div className="event__people">
+                  <span>+{item.people}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="bottom__left--bottom">
+            <button>See More</button>
+          </div>
+        </div>
+
+        <div className="calender__bottom--right">
+          <div className="bottom__right--top">
+            <h4>Today</h4>
+            <h2>October 2026</h2>
+
+            <div className="bottom--right--top__selects">
+              <button>Day</button>
+              <button>Week</button>
+              <button className="active">Month</button>
+            </div>
+          </div>
+
+          <div className="bottom__right--calendar">
+            <img src={calendarData.calendarImage} alt="calendar" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
